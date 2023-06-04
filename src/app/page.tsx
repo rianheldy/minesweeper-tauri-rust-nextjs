d8eeebec2562fa66e4e96f30ac4a9c471c443394
heldysetiawan <96930153+heldysetiawan@users.noreply.github.com>
2023-06-04 15:16:55 +0700

    add tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+//import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      
+    <main>
+      <div className='bg-teal-700 w-screen h-screen flex flex-row flex-nowrap items-center justify-center'>
+
+      </div>
     </main>
   )
 }

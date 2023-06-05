ba332a48c76660a8914b7c94728c87dc646690d3
rianheldy <rian.heldy@gmail.com>
2023-06-05 20:31:50 +0700

    increase font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className='relative'>
       <section className='transition-all relative w-72 shadow-myboxshadow rounded-2xl z-10'>
         <header className='relative bg-[rgba(255,255,255,.2)] pt-7 pb-3 pl-7 pr-7 text-right rounded-tl-2xl rounded-tr-2xl backdrop-blur-2xl shadow-header-shadow'>
-          <div className='text-lg mb-1'>5874 + 2123</div>
+          <div className='text-xl mb-1'>5874 + 2123</div>
         </header>
       </section>
     </main>
